9f9bb044365c731a3db90c089732c90c4123c37a
Aishwarya Murali <Aishwarya@NTP-LAP-1173.chn.nestgroup.net>
2023-02-05 12:30:08 +0530

    7154

---
+++ src/main/java/com/nest/bluehydrogen/kafkaFlinkMongo/flink/FlinkConsumer.java
@@ -0,0 +1,52 @@
+package com.nest.bluehydrogen.kafkaFlinkMongo.flink;
+
+import com.nest.bluehydrogen.kafkaFlinkMongo.sink.MongoDbSink;
+import lombok.Getter;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.springframework.beans.factory.annotation.Value;
+import org.bson.Document;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+@Component
+@Getter
+public class FlinkConsumer {
+    @Value("${kafka.bootstrap.servers}")
+    private String kafkaBootstrapServers;
+
+    @Value("${kafka.group.id}")
+    private String kafkaGroupId;
+
+    @Value("${kafka.zookeeper.connect}")
+    private String kafkaZookeeperConnect;
+
+    @Value("${kafka.topic}")
+    private String kafkaTopic;
+
+    @Value("${mongodb.uri}")
+    private String mongodbUri;
+
+    @Value("${mongodb.database}")
+    private String mongodbDatabase;
+
+    @Value("${mongodb.collection}")
+    private String mongodbCollection;
+    @Value("${flink.job.name}")
+    private String flinkJobName;
+    public void start() throws Exception{
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        Properties consumerProperties = new Properties();
+        consumerProperties.setProperty("bootstrap.servers", kafkaBootstrapServers);
+        consumerProperties.setProperty("group.id", kafkaGroupId);
+        consumerProperties.setProperty("zookeeper.connect", kafkaZookeeperConnect);
+        FlinkKafkaConsumer<String> consumer = new FlinkKafkaConsumer<>(kafkaTopic, new SimpleStringSchema(), consumerProperties);
+        consumer.setStartFromEarliest();
+        DataStream<String> dataStream = env.addSource(consumer);
+        DataStream<Document> transformedDataStream = dataStream.map(value -> Document.parse(value));
+        transformedDataStream.addSink(new MongoDbSink(mongodbUri, mongodbDatabase, mongodbCollection));
+        env.execute(flinkJobName);
+    }
+}
---
+++ src/test/java/com/nest/bluehydrogen/kafkaFlinkMongo/KafkaFlinkMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.bluehydrogen.kafkaFlinkMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaFlinkMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
